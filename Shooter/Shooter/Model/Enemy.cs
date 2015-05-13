7c18eb070cd2d36da6ae30240fcf2bd7109ab6b2
DavisKerr <davis.kerr1997@gmail.com>
2015-05-13 13:44:23 -0600

    Added to git

---
+++ Shooter/Shooter/Model/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Shooter.View;
+
+namespace Shooter.Model
+{
+    class Enemy
+    {
+        public Animation EnemyAnimation;
+        public Vector2 Position;
+        public bool Active;
+        public int Health;
+        public int Damage;
+        public int Value;
+        float enemyMoveSpeed;
+        public int Width
+        {
+            get { return EnemyAnimation.FrameWidth; }
+        }
+        public int Height
+        {
+            get { return EnemyAnimation.FrameHeight; }
+        }
+
+        public void Initialize(Animation animation, Vector2 position)
+        {
+            // Load the enemy ship texture
+            EnemyAnimation = animation;
+
+            // Set the position of the enemy
+            Position = position;
+
+            // We initialize the enemy to be active so it will be update in the game
+            Active = true;
+
+
+            // Set the health of the enemy
+            Health = 10;
+
+            // Set the amount of damage the enemy can do
+            Damage = 10;
+
+            // Set how fast the enemy moves
+            enemyMoveSpeed = 6f;
+
+
+            // Set the score value of the enemy
+            Value = 100;
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // The enemy always moves to the left so decrement it's xposition
+            Position.X -= enemyMoveSpeed;
+
+            // Update the position of the Animation
+            EnemyAnimation.Position = Position;
+
+            // Update Animation
+            EnemyAnimation.Update(gameTime);
+
+            // If the enemy is past the screen or its health reaches 0 then deactivateit
+            if (Position.X < -Width || Health <= 0)
+            {
+                // By setting the Active flag to false, the game will remove this objet fromthe 
+                // active game list
+                Active = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the animation
+            EnemyAnimation.Draw(spriteBatch);
+        }
+
+    }
+}
---
+++ Shooter/Shooter/Model/Player.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Shooter.View;
+
+namespace Shooter.Model
+{
+    public class Player
+    {
+        private Texture2D playerTexture;
+        public Vector2 Position;
+        private int health;
+        private bool active;
+        private int weapon;
+
+        
+
+
+
+        public Animation PlayerAnimation;
+
+        public Texture2D PlayerTexture
+        {
+            get { return playerTexture; }
+            set { playerTexture = value; }
+        }
+
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+        public int Width
+        {
+            get {return PlayerAnimation.FrameWidth;}
+        }
+
+        public int Height
+        {
+            get {return PlayerAnimation.FrameHeight;}
+        }
+
+        public int Weapon
+        {
+            get { return weapon; }
+            set { weapon = value; }
+        }
+
+
+
+        public void Initialize(Animation animation, Vector2 position)
+        {
+            PlayerAnimation = animation;
+            Position = position;
+            Active = true;
+            weapon = 1;
+            Health = 100;
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            PlayerAnimation.Position = Position;
+            PlayerAnimation.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            PlayerAnimation.Draw(spriteBatch);
+            //spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+
+
+
+    }
+}
